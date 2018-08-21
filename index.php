dd6ffcb7f6d6bbf240a1ca9829a892b12ac89e7f
Hudson Dionizio <hudsondionizio@hotmail.com>
2018-08-21 16:00:45 -0300

    inicio

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="resources/css/bootstrap.css">
+    <link rel="stylesheet" href="resources/">
+    <title>Seja-Bem-Vindo</title>
+</head>
+<body>
+<nav class="navbar navbar-light bg-light"><!--Barra de Navegação-->
+     <a class="navbar-brand">Banco</a>
+        <form class="form-inline">
+            <input class="form-control mr-sm-2" type="text" placeholder="Usuario" aria-label="Usuario">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+        </form>
+</nav>
+<div class="container"> <!--Início do container-->
+    
+</div> <!--Fim do container -->
+</body>
+</html>
